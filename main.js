65bacbcdd54aaa8c4bee6608224b58236a7d1f3e
NatiTesfay <netaneltes66@gmail.com>
2022-05-22 22:40:37 +0300

    19.05.2022--j.s

---
+++ main.js
@@ -0,0 +1,110 @@
+// function namesArray(myString) {
+//     for (let i = 0; i < myString.length; i++) {
+// if (condition) {
+    
+// } else {
+    
+// }
+
+//     }
+// }
+
+
+// function funcNum() {
+//   var array = ['javascript'];
+//   for (let i = 0; i < array.length; i++) {
+//     if ( array[i] < 0) {
+//       return array;
+//     }
+//     console.log(array)
+   
+//   }
+ 
+// }
+// funcNum();
+
+
+// function funcNum() {
+//   var array = ['1,5,80,10'];
+//   for (let i = 0; i < array.length; i++) {
+//     if ( array[i] < 0) {
+//     }
+  
+//     console.log(array.toString(array));
+//   }return array;
+// }
+// funcNum();
+
+
+// var array = (name:'natan' , family:'krasney' , city:'haifa'); //to finish
+
+// function myKeyValue(str) {
+//   var startstr =str[0]; var psik = 0;
+//   for (let i = 0; i < str.length; i++) {  
+//       if (str[i] == ",") {
+//          if(startstr[i].lenght < Str.subString(psik,i).lenght){
+//          startstr = str.subString(psik ,i);
+//         psik = i + 1; // מקדמים את פסיק פלוס אחד 
+//          }
+//       }
+//   }
+//   console.log(str);
+//   return startstr;
+// }
+// myKeyValue(str);
+
+
+// function myString() {
+//   var array = 'hello';
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] < array) {
+//      return;
+//     }
+//     else(console.log('short')
+//       )
+//   }
+// }
+// myString();
+
+
+// function myString() {
+//   var array = ['hello', 52];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < array) {
+//       return array;
+//     }console.log('index bigger');
+     
+    
+//   }
+  
+  
+// }
+
+
+// function myString() {
+//   var name = 'sayonara';
+//   var result = name.substring(0,3)
+//   for (let i = 0; i < name.length; i++) {
+//    if (name[i] != 0) {
+     
+//    } 
+
+//   }console.log(result);
+  
+// }
+// myString();
+
+
+
+function myKey(str) {
+  var array = 'sayonara';
+  for (let i = 0; i < array.length; i++) {
+   if (array[i]== array.length) {
+   
+   } 
+   console.log (array.substring(2,4))
+  }
+  return array;
+
+}
+myKey(str);
